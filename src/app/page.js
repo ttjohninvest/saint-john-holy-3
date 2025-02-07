9e59f0fdff7330e292604cc21f871f0d1928c384
John <jmjohnmcgovern707@gmail.com>
2025-02-06 19:16:49 -0800

    A picture of my precious Jesus Christ at 12 years old with the teachers

--- src/app/page.js
+++ src/app/page.js
@@ -129,9 +129,9 @@ export default function page({ children }) {
             Jesus Christ. Amen. Yes. Movies I saw recently and I love them both,
             The Gospel of John, English Version The song of Saint Bernadette.
           </div>
-          <div><img width="100" height="100" className="w-full mt-8" src="/Precious-Jesus-with-the-teachers.png" alt="Precious Jesus Christ with the teachers"></img></div>
-          <div><img width="100" height="100" className="w-full mt-4" src="/saint-peters-basilica-1.png" alt="Saint Peter's Basilica"></img></div>
-          <div><img width="100" height="100" className="w-full mt-4" src="/the-Cross-in-Our-Lady-Of-Peace-in-Honolulu.png" alt=""></img></div>
+          <div><img width="100" height="100" className="w-full mt-8" src="/Precious-Jesus-with-the-teachers.png" alt="Precious Jesus Christ with the teachers" title="A picture of my precious Jesus Christ at 12 years old with the teachers."></img></div>
+          <div><img width="100" height="100" className="w-full mt-4" src="/saint-peters-basilica-1.png" alt="Saint Peter's Basilica" title="Saint Peter's Basilica"></img></div>
+          <div><img width="100" height="100" className="w-full mt-4" src="/the-Cross-in-Our-Lady-Of-Peace-in-Honolulu.png" alt="" title="The cross in the Catholic Church of Our Lady of Peace in the location where my precious Jesus Christ healed my eyes in Honolulu."></img></div>
         </div>
       </div>
     </div>
