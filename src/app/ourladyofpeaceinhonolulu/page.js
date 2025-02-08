53054f031da58467749c22513b75c6fbfb11502d
John <jmjohnmcgovern707@gmail.com>
2025-02-07 17:56:52 -0800

    margin top align the movies

--- src/app/ourladyofpeaceinhonolulu/page.js
+++ src/app/ourladyofpeaceinhonolulu/page.js
@@ -1,3 +1,3 @@
 export default function Page() {
-    return <div className=""><iframe width="560" height="315" className="w-full h-[100vh]" src="https://www.youtube.com/embed/aEG9-qfsyXs?si=F0SQSfSrB3uAhtS_" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe></div>
+    return <div className="mt-[78px] lg:mt-[51px]"><iframe width="560" height="315" className="w-full h-[100vh]" src="https://www.youtube.com/embed/aEG9-qfsyXs?si=F0SQSfSrB3uAhtS_" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe></div>
 }
--- src/app/queens-funeral/page.js
+++ src/app/queens-funeral/page.js
@@ -3,6 +3,6 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div><iframe width="560" height="315" className="w-full h-[100vh]" src="https://www.youtube.com/watch?v=YlNGjd90gp4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe></div>
+    <div className="mt-[78px] lg:mt-[51px]"><iframe width="560" height="315" className="w-full h-[100vh]" src="https://www.youtube.com/watch?v=YlNGjd90gp4" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe></div>
   )
 }
