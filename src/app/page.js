125aaf8153a8999b0af3c627530095c4160ca473
John <jmjohnmcgovern707@gmail.com>
2025-02-08 15:08:53 -0800

    two more videos

--- src/app/page.js
+++ src/app/page.js
@@ -200,6 +200,30 @@ export default function page({ children }) {
             
             </div>
           </div>
+
+          <div>
+            <div
+              className="relative max-w-xs- w-full overflow-hidden bg-cover bg-no-repeat mt-4"
+              data-twe-ripple-init
+              data-twe-ripple-color="light"
+            >
+              <img
+                src="/singing-of-the-holy-rosary.png"
+                className="max-w-xs- w-full"
+                alt="Louvre"
+              />
+
+              {/* <a href="https://www.youtube.com/watch?v=gE8EDuVR97M">
+                <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+              </a> */}
+
+              
+              <Link href="/singing-of-the-holy-rosary" title="Singing of the Holy Rosary">
+              <div className="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsl(0,0%,98.4%,0.2)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100"></div>
+              </Link>
+            
+            </div>
+          </div>
          
         </div>
       </div>
